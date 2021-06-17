ca2f4f74caf268139fcba16fc7769a2b4ee3fc71
rafaballerini <rafaella_ballerini@hotmail.com>
2021-06-17 12:48:33 -0300

    identação

--- projeto/src/components/Form/index.tsx
+++ projeto/src/components/Form/index.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 
-function Form () {
+function Form() {
 
-    function handleOnSubmit(event: React.FormEvent<HTMLFormElement>){
-        console.log("tarefa cadastrada")
-        event.preventDefault()
-    }
+  function handleOnSubmit(event: React.FormEvent<HTMLFormElement>) {
+    console.log("tarefa cadastrada")
+    event.preventDefault()
+  }
 
-    return (
-        <form onSubmit={handleOnSubmit}>
-            <div className="inputContainer">
-                <label htmlFor="task">Nova tarefa</label>
-                <input type="text" name="task" id="task" placeholder="O que você estudará?" required/>
-            </div>
-            <div className="inputContainer">
-                <label htmlFor="time">Tempo</label>
-                <input type="time" name="time" id="time" placeholder="00:00" required/>
-            </div>
-            <button type="submit" style={{ display: 'none'}}>Confirmar Tarefa</button>
-        </form>
-    )
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div className="inputContainer">
+        <label htmlFor="task">Nova tarefa</label>
+        <input 
+          type="text" 
+          name="task" 
+          id="task" 
+          placeholder="O que você estudará?" 
+          required />
+      </div>
+      <div className="inputContainer">
+        <label htmlFor="time">Tempo</label>
+        <input 
+          type="time" 
+          name="time" 
+          id="time" 
+          placeholder="00:00" 
+          required />
+      </div>
+      <button 
+        type="submit" 
+        style={{ display: 'none' }}>
+          Confirmar Tarefa
+      </button>
+    </form>
+  )
 }
 
 export default Form;
